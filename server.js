2084074123fb42e7370eaa35128cb96b6c62239b
Tobias Andersen <dappjump@gmail.com>
2020-07-06 23:14:49 +0200

    update port var

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const axios = require('axios')
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000
 const bodyParser = require('body-parser')
 const cors = require('cors')
 app.use(cors())
